40d0b8f16410f6f2a2569d73ce2d12adf973d0ea
mohamedwshaalan <mohamedshaalan@aucegypt.edu>
2022-08-10 19:07:40 +0200

    Final calculator design + start of js logic

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("Testing")
+console.log("Testing");
+let firstOperand = '';
+let secondOperand = '';
+let currentOperator = null;
+let displayValue = '';
+let resetScreen = false;
+
+function operate(operation, operandone, operandtwo) {
+
+}
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+const divide = function (a, b) {
+  return a / b;
+}
+const sum = function (array) {
+  return array.reduce((total, current) => total + current, 0);
+};
+
+const multiply = function (array) {
+  return array.length
+    ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+    : 0;
+};
+
+const power = function (a, b) {
+  return Math.pow(a, b);
+};
